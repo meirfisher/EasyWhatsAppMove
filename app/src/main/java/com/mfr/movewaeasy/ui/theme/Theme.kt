24ad29c6a1c7345afc6b1af397e71adf5596cd95
meirfisher <mfr5995@gmail.com>
2025-02-27 19:05:00 +0200

    Initial project setup and update versions

--- app/src/main/java/com/mfr/movewaeasy/ui/theme/Theme.kt
+++ app/src/main/java/com/mfr/movewaeasy/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.mfr.movewaeasy.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
