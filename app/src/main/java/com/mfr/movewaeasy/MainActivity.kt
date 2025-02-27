3cd0e960dc255e75bec6f467d683c8f6b86fb60b
meir fisher <mfr5995@gmail.com>
2025-02-28 01:29:40 +0200

    Updated MainActivity with navigation

--- app/src/main/java/com/mfr/movewaeasy/MainActivity.kt
+++ app/src/main/java/com/mfr/movewaeasy/MainActivity.kt
@@ -3,45 +3,25 @@ package com.mfr.movewaeasy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.mfr.movewaeasy.ui.theme.MoveWhatsAppEasyTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mfr.movewaeasy.ui.theme.MoveWAEasyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            MoveWhatsAppEasyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            MoveWAEasyTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("permission") { PermissionScreen(navController) }
+                    composable("main") { MainScreen(navController) }
+                    composable("backup") { BackupScreen(navController) }
+                    composable("restore") { RestoreScreen(navController) }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoveWhatsAppEasyTheme {
-        Greeting("Android")
-    }
-}
