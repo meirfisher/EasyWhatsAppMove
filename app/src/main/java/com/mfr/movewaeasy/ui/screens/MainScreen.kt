5c9e99e13e3a1bf78c85b25ad1a0b284c986ea26
meir fisher <mfr5995@gmail.com>
2025-03-14 01:52:20 +0200

    make th MainScreen nicer with cards

--- app/src/main/java/com/mfr/movewaeasy/ui/screens/MainScreen.kt
+++ app/src/main/java/com/mfr/movewaeasy/ui/screens/MainScreen.kt
@@ -1,30 +1,118 @@
 package com.mfr.movewaeasy.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun MainScreen(navController: NavController) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text("Im in my old Device")
-        Button(
-            onClick = { navController.navigate("Backup") },
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Text("Backup")
+fun MainScreen(navController: NavController = NavController(LocalContext.current)) {
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Move WhatsApp Easy") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            )
         }
-        Text("Im in my new Device")
-        Button(
-            onClick = { navController.navigate("Restore") },
-            modifier = Modifier.padding(8.dp)
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(10.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(2.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Restore")
+            Spacer(modifier = Modifier.height(26.dp))
+            MainScreenContent(navController)
+        }
+    }
+}
+
+@Composable
+fun MainScreenContent(navController: NavController) {
+
+    CardComponent(
+        title = "I'm in my Old Device",
+        description = "Create a backup of your WhatsApp media",
+        onClick = { navController.navigate("Backup") }
+    )
+
+    Spacer(modifier = Modifier.padding(16.dp))
+
+    CardComponent(
+        title = "I'm in my New Device",
+        description = "Restore your WhatsApp media from a backup",
+        onClick = { navController.navigate("Restore") }
+    )
+}
+
+@Composable
+fun CardComponent(title: String, description: String, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            // Adjust the height to make the card Exactly half the screen height
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                onClick = onClick,
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Backup",
+                        modifier = Modifier.padding(end = 18.dp)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("Backup")
+                }
+            }
         }
     }
 }
