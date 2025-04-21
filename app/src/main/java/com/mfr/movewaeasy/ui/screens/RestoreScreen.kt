20a392b9412491948e65f5f3deb8d974dc904353
meir fisher <mfr5995@gmail.com>
2025-04-21 21:53:10 +0300

    feat: Improve UI and functionality of RestoreScreen
    - Refactored RestoreScreen to use cards for displaying file details and restore progress.
    - Improved UI layout and added MaterialTheme styling for better aesthetics.
    - Added a ConfirmationDialog to handle cases where a WhatsApp folder is found, ensuring data is restored to "WhatsApp-Backup" to avoid overwriting.
    - Enhanced error handling by displaying error messages below the file selection button.
    - Updated progress display with clear text indicators for progress percentage and file restoration status.
    - Added a Cancel button to stop the restore process.
    - Improved readability of file details by displaying them in a structured card format.

--- app/src/main/java/com/mfr/movewaeasy/ui/screens/RestoreScreen.kt
+++ app/src/main/java/com/mfr/movewaeasy/ui/screens/RestoreScreen.kt
@@ -3,6 +3,7 @@ package com.mfr.movewaeasy.ui.screens
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,14 +12,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,65 +30,31 @@ import com.mfr.movewaeasy.utils.FileUtils.toStringSize
 import com.mfr.movewaeasy.viewmodels.RestoreViewModel
 
 @Composable
-fun RestoreScreen() {
+fun RestoreScreen(viewModel: RestoreViewModel = viewModel()) {
     val context = LocalContext.current
-    val viewModel: RestoreViewModel = viewModel()
     val state by viewModel.state.collectAsState()
-    val showConfirmationDialog = remember { mutableStateOf(false) }
+    var showConfirmationDialog by remember { mutableStateOf(false) }
     val launcher = rememberLauncherForActivityResult(
         ActivityResultContracts.GetContent()
     ) { uri ->
         uri?.let { viewModel.setBackupFile(uri = it, context = context) }
     }
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(onClick = { launcher.launch("application/zip") }) {
             Text("Select Backup File")
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
         if (state.isFileSelected) {
-            Text("Selected File: ${state.fileName}")
-            Text("File Size: ${state.fileSize.toStringSize()}")
-            Text("Creation Time: ${state.creationTime}")
+            FileDetailsCard(state)
             if (state.isRestoring) {
-                Column(modifier = Modifier.padding(vertical = 8.dp)) {
-                    Text(
-                        text = "Progress: ${(state.progress * 100).toInt()}%",
-                        modifier = Modifier.padding(top = 4.dp)
-                    )
-                    LinearProgressIndicator(
-                        progress = { state.progress },
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Text("Restoring...")
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(250.dp)
-                            .padding(8.dp),
-                        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-                        colors = CardDefaults.cardColors(containerColor = Color.Black)
-                    ) {
-                        Column(
-                            modifier = Modifier.padding(16.dp),
-                            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
-                        ) {
-                            Text(
-                                text = "Files restored: ${state.filesRestoredCount} of ${state.filesTotalCount}",
-                                color = Color.White
-                            )
-                            Text(
-                                text = "Restoring ${state.fileOnProgress}",
-                                color = Color.White
-                            )
-                        }
-                    }
-                    Button(
-                        onClick = { viewModel.cancelRestore() },
-                        modifier = Modifier.padding(vertical = 8.dp)
-                    ) {
-                        Text("Cancel Restore")
-                    }
-                }
+                RestoreProgress(state, onCancel = viewModel::cancelRestore)
             } else {
                 Button(
                     onClick = {
@@ -94,7 +63,7 @@ fun RestoreScreen() {
                         }
                         else {
                             // Raise a confirmation dialog or similar to ask for confirmation
-                            showConfirmationDialog.value = true
+                            showConfirmationDialog = true
                         }
                     },
                     modifier = Modifier.padding(vertical = 8.dp)
@@ -102,38 +71,110 @@ fun RestoreScreen() {
                     Text("Restore")
                 }
             }
-            if (showConfirmationDialog.value) {
-                AlertDialog(
-                    onDismissRequest = { showConfirmationDialog.value = false },
-                    title = { Text("Confirm Restore") },
-                    text = { Text("A WhatsApp folder was found. To avoid overwrite existing data, the data will be restored to \"WhatsApp-Backup\" folder. Are you sure you want to continue?") },
-                    confirmButton = {
-                        Button(onClick = {
-                            showConfirmationDialog.value = false
-                            viewModel.restoreFile(context = context)
-                        }) {
-                            Text("Confirm")
-                        }
-                    },
-                    dismissButton = {
-                        Button(onClick = { showConfirmationDialog.value = false }) {
-                            Text("Cancel")
-                        }
-                    }
-                )
+            if (showConfirmationDialog) {
+               ConfirmationDialog(
+                   onConfirm = {
+                       showConfirmationDialog = false
+                       viewModel.restoreFile(context = context)
+                   },
+                   onDismiss = { showConfirmationDialog = false }
+               )
             }
         } else {
-            Text("please select a valid backup file")
+            Text("please select a valid backup file", style = MaterialTheme.typography.bodyMedium)
         }
 
         state.errorMessage?.let {
             Text(
                 text = it,
-                color = Color.Red,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodySmall,
                 modifier = Modifier.padding(top = 8.dp)
             )
         }
     }
+}
+
+@Composable
+private fun FileDetailsCard(state: RestoreViewModel.RestoreState) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Selected File: ${state.fileName ?: "None"}", style = MaterialTheme.typography.bodyMedium)
+            Text("File Size: ${state.fileSize.toStringSize()}", style = MaterialTheme.typography.bodyMedium)
+            Text("Created: ${state.creationTime ?: "N/A"}", style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
+
+@Composable
+private fun RestoreProgress(state: RestoreViewModel.RestoreState, onCancel: () -> Unit) {
+    Card(modifier = Modifier.padding(vertical = 8.dp)) {
+        Text(
+            text = "Progress: ${(state.progress * 100).toInt()}%",
+            style = MaterialTheme.typography.bodyMedium
+        )
+        LinearProgressIndicator(
+            progress = { state.progress },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        Text(
+            text = "Restoring...",
+            style = MaterialTheme.typography.bodyMedium
+        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(vertical = 8.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Files restored: ${state.filesRestoredCount}/${state.filesTotalCount ?: "N/A"}",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+        Button(onClick = onCancel, modifier = Modifier.padding(vertical = 8.dp)) {
+            Text("Cancel Restore Process")
+        }
+    }
+}
+
+@Composable
+private fun ConfirmationDialog(onConfirm: () -> Unit, onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = { Text("Confirm Restore") },
+        text = {
+            Text(
+                "A WhatsApp folder was found. Data will be restored to \"WhatsApp-Backup\" Folder to avoid overwriting. Are you sure you want to continue?"
+            )
+        },
+        confirmButton = {
+            Button(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        }
+    )
+
 }
 
 @Preview(showBackground = true)
